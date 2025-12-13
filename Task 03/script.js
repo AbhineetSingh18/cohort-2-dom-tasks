7f79e37df3dbf1a87e3866570d5697bcf59081aa
Abhineet Singh <singhabhineet72@gmail.com>
2025-12-13 23:13:35 +0530

    first commit

---
+++ Task 03/script.js
@@ -0,0 +1,45 @@
+var arr = [
+    {
+        team : 'RCB',
+        fullname : 'Royal Challengers Banglore',
+        captain : 'Rajat Patidar'
+    },
+    {
+        team : 'CSK',
+        fullname : 'Chennai Super Kings',
+        captain : 'Rituraj Gaikwad'
+
+    },
+    {
+        team : 'RR',
+        fullname : 'Rajasthan Royals'
+    },
+    {
+        team : 'MI',
+        fullname : 'Mumbai Indians'
+    },
+    {
+        team : 'DC',
+        fullname : 'Delhi Capitals'
+    },
+    {
+        team : 'PBKS',
+        fullname : 'Punjab Kings'
+    },
+    {
+        team : 'KKR',
+        fullname : 'Kolkata Knight Riders'
+    },
+    {
+        team : 'SRH',
+        fullname : 'Sunrisers Hyderabad'
+    },
+    {
+        team : 'LSG',
+        fullname : 'Lucknow Super Giants'
+    },
+    {
+        team : 'GL',
+        fullname : 'Gujrat Titans'
+    },
+]
