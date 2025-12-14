548b66e7293e7e429819bb08d7c1508fda7213b0
Abhineet Singh <singhabhineet72@gmail.com>
2025-12-14 22:25:37 +0530

    second commit

--- Task 03/script.js
+++ Task 03/script.js
@@ -1,45 +1,22 @@
-var arr = [
-    {
-        team : 'RCB',
-        fullname : 'Royal Challengers Banglore',
-        captain : 'Rajat Patidar'
-    },
-    {
-        team : 'CSK',
-        fullname : 'Chennai Super Kings',
-        captain : 'Rituraj Gaikwad'
+var btn = document.querySelector('#btn');
+var main = document.querySelector('main');
+var arr = ['Code is today’s universal language.','Technology is best when it brings people together.','Think logically, build digitally.','Software eats the world.','Tech turns imagination into reality.','Build once, scale endlessly.','Automation is the future in motion.'];
 
-    },
-    {
-        team : 'RR',
-        fullname : 'Rajasthan Royals'
-    },
-    {
-        team : 'MI',
-        fullname : 'Mumbai Indians'
-    },
-    {
-        team : 'DC',
-        fullname : 'Delhi Capitals'
-    },
-    {
-        team : 'PBKS',
-        fullname : 'Punjab Kings'
-    },
-    {
-        team : 'KKR',
-        fullname : 'Kolkata Knight Riders'
-    },
-    {
-        team : 'SRH',
-        fullname : 'Sunrisers Hyderabad'
-    },
-    {
-        team : 'LSG',
-        fullname : 'Lucknow Super Giants'
-    },
-    {
-        team : 'GL',
-        fullname : 'Gujrat Titans'
-    },
-]
+btn.addEventListener('click', function(){
+    var h2 = document.createElement('h2');
+    var a = Math.floor(Math.random()*arr.length);
+    var x = Math.random()*100;
+    var y = Math.random()*100;
+    var r = Math.floor(Math.random()*360);
+    var c1 = Math.floor(Math.random()*255);
+    var c2 = Math.floor(Math.random()*255);
+    var c3 = Math.floor(Math.random()*255);
+
+    h2.innerHTML = arr[a];
+    h2.style.position = 'absolute'
+    h2.style.top = x+'%';
+    h2.style.left = y+'%';
+    h2.style.rotate = r+'deg'
+    h2.style.color = `rgb(${c1},${c2},${c3})`
+    main.appendChild(h2);
+})
